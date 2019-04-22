0d1932c8af8a68304ed71334fe7a51a1aad26b64
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-04-22 10:48:01 -0400

    completed pushUp method

--- MyHeap.java
+++ MyHeap.java
@@ -1,17 +1,19 @@
 public class MyHeap{
   public static void heapify(int[] ary){}
 
-  private static void pushUp(int[] ary, int size, int index){
+  private static void pushDown(int[] ary, int size, int index){
     if(index * 2 + 1 > size){
       return;}
     if(index * 2 + 2 > size){
       if(ary[index] < ary[index * 2 + 1]){
         swap(ary, index, index * 2 + 1);}
       return;}
+    if(ary[index] > ary[index * 2 + 1] && ary[index] > ary[index * 2 + 2]){
+      return;}
     if(ary[index] < ary[index * 2 + 1] && ary[index] < ary[index * 2 + 2]){
       swap(ary, index, max(ary[index * 2 + 1], ary[index * 2 + 2]));
       index = index * 2 + 1;}
-    pushUp(ary, size, index);}
+    pushDown(ary, size, index);}
 
   private static void swap(int[] ary, int ind1, int ind2){
     int temp = ary[ind2];
@@ -23,7 +25,12 @@ public class MyHeap{
       return b;}
     return a;}
 
-
+  private static void pushUp(int[] ary, int index){
+    if(index == 0 || ary[(index - 1) / 2] > ary[index]){
+      return;}
+    swap(ary, index, (index - 1) / 2);
+    pushUp(ary, (index - 1) / 2);
+  }
 
 
 }
